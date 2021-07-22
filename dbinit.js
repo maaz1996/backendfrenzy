e6805b6e1b541968a6db40eb9b9a94c1b756c008
maaz1996 <maaz.abdullah111@gmail.com>
2021-07-22 10:59:27 +0530

    db table initialization script complete

--- dbinit.js
+++ dbinit.js
@@ -7,8 +7,6 @@ let url = 'https://gist.githubusercontent.com/seahyc/de33162db680c3d595e95575217
   fetch(url)
   .then(res => res.json())
   .then(async(out) => {
-    out = out.slice(0, 1);
-    console.log("welcome here to data", out)
     await db.createTable('purchaseHistory')
     await db.createTable('User')
     // console.log('Checkout this JSON! ',out);
@@ -29,7 +27,7 @@ let url = 'https://gist.githubusercontent.com/seahyc/de33162db680c3d595e95575217
               var savedata=  await db.saveData({
                 ...purchasepayload,
               }, 'purchaseHistory');
-              console.log(savedata.purchase_id)
+              
               return(savedata.purchase_id)
           }))
           
@@ -41,7 +39,7 @@ let url = 'https://gist.githubusercontent.com/seahyc/de33162db680c3d595e95575217
                 }, 'User');
               
     })
-  })
+  }).then(data=>console.log("data inserted"))
   .catch(err => { throw err });
 
 
@@ -50,8 +48,6 @@ let url = 'https://gist.githubusercontent.com/seahyc/de33162db680c3d595e95575217
   fetch(url2)
   .then(res2 => res2.json())
   .then(async(out2) => {
-    out2 = out2.slice(0, 2);
-    console.log("welcome here to data2", out2)
     await db.createTable('Menu')
     await db.createTable('Restaurant')
     // console.log('Checkout this JSON! ',out);
@@ -66,11 +62,10 @@ let url = 'https://gist.githubusercontent.com/seahyc/de33162db680c3d595e95575217
                 price:j.price,
                 
               }
-              // const createtable=await db.createTable('purchaseHistory')
               var savedata=  await db.saveData({
                 ...menupayload,
               }, 'Menu');
-              console.log(savedata.id)
+              
               return(savedata.id)
           })),
           openingHours:i.openingHours,
@@ -84,5 +79,5 @@ let url = 'https://gist.githubusercontent.com/seahyc/de33162db680c3d595e95575217
                 }, 'Restaurant');
               
     })
-  })
+  }).then(data=>console.log("data inserted"))
   .catch(err => { throw err });
